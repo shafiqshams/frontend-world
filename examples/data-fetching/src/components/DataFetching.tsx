6731f9f26700472db778b506a84c323b40aa700d
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-04-12 16:19:11 +0200

    feat: use custom hook in data fetching component

--- examples/data-fetching/src/components/DataFetching.tsx
+++ examples/data-fetching/src/components/DataFetching.tsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../config/appConfig";
-
-
-interface Post {
-   id: number;
-   title: string;
-}
+import { usePosts } from "../hooks/usePosts";
 
 const DataFetching = () => {
 
-   const [posts, setPosts] = useState<Post[]>([]);
-   const [isLoading, setIsLoading] = useState<boolean>(false);
-   const [error, setError] = useState<Error | null>(null);
-
-   const fetchPost = async () => {
-      setIsLoading(true)
-
-      try {
-         const response = await fetch(`${BASE_URL}/posts`);
-         if (!response.ok) {
-            throw new Error("HTTP error, request failed")
-         }
-         const posts = (await response.json()) as Post[]
-         setPosts(posts)
-      } catch (e) {
-         const error = e instanceof Error ? e : new Error("Something went wrong");
-         setError(error)
-      } finally {
-         setIsLoading(false)
-      }
-
-   }
-
-   useEffect(() => {
-      fetchPost();
-   }, [])
+   const { posts, isLoading, error } = usePosts();
 
    return (
       <div>
