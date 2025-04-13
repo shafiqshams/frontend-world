255513e1e2d69e1c4cada0ada06a0229acc9a1bc
Shafiq Shams <shafiqshamsessani@gmail.com>
2025-04-13 19:21:14 +0200

    feat: add filtering post functionality

--- examples/data-fetching/src/components/DataFetching.tsx
+++ examples/data-fetching/src/components/DataFetching.tsx
@@ -1,22 +1,24 @@
+import { useState } from "react";
 import { usePosts } from "../hooks/usePosts";
 import Error from "./Error";
 import Loading from "./Loading";
 import PostsList from "./PostsList";
-const DataFetching = () => {
+import { SearchBar } from "./SearchBar";
+import { filterPosts } from "../utils/filterPosts";
 
+const DataFetching = () => {
    const { posts, isLoading, error, refetch } = usePosts();
+   const [search, setSearch] = useState<string>('');
+
+   const visiblePosts = filterPosts(posts, search);
 
    return (
       <div>
          <h3>Data Fetching</h3>
          {isLoading && <Loading />}
-         {error &&
-            <>
-               <Error error={error} />
-               <button onClick={refetch}>Retry</button>
-            </>
-         }
-         <PostsList posts={posts} />
+         {error && <Error error={error} refetch={refetch} />}
+         <SearchBar value={search} onChange={setSearch} />
+         <PostsList posts={visiblePosts} />
       </div>
    )
 }
